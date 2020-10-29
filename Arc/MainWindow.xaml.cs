fdeaa29b6a55fa6215c80f180c5dde2f58303358
Captain Newt <42378068+NNeerly@users.noreply.github.com>
2020-10-30 03:32:01 +0800

    Second Commit

--- Arc/MainWindow.xaml.cs
+++ Arc/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Arc.Xml;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,7 +23,7 @@ namespace Arc
     {
         public MainWindow()
         {
-            InitializeComponent();
+            InitializeComponent(); 
         }
     }
 }
---
+++ Arc/Xml/SongData.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arc.Xml
+{
+    public class SongData
+    {
+        public string Title { get; private set; }
+        public string Author { get; private set; }
+        public List<string> Tags;
+        public List<SongLyric> Lyric;
+
+        public SongData(string title, string author)
+        {
+            Title = title;
+            Author = author;
+            Tags = new List<string>();
+            Lyric = new List<SongLyric>();
+        }
+    }
+
+    public class SongLyric
+    {
+        public SongLyricType Type;
+        public string Text;
+    }
+
+    public enum SongLyricType
+    {
+        Stanza,
+        Chorus,
+        Bridge
+    }
+}
