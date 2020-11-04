c925fa69416d8d8d074c322332b6eae3738c947e
Christian Stockinger <c.stockinger@intence.de>
2020-11-04 09:30:27 +0100

    add UserControl

--- Arc/MainWindow.xaml.cs
+++ Arc/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using Arc.Xml;
 using System.Windows;
+using Arc.UserControls;
 
 namespace Arc
 {
@@ -9,9 +10,21 @@ namespace Arc
     /// </summary>
     public partial class MainWindow : Window
     {
+        public List<MainMenuItem> MenuItems { get; set; }
         public MainWindow()
         {
+            FillMenuList();
             InitializeComponent();
+            MenuItemsListBox.SelectedIndex = 0;
+
+        }
+
+        private void FillMenuList()
+        {
+            MenuItems = new List<MainMenuItem>()
+            {
+                new MainMenuItem("/Resources/Images/Music_White.png",new SongLibrary())
+            };
         }
     }
 }
