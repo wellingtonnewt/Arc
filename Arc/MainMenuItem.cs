9155d92a2fa7fa67d2987e84356e5ce566f8b21d
Stockinger Christian <mail@christian-stockinger.de>
2020-11-05 21:06:03 +0100

    Add Bindings

---
+++ Arc/MainMenuItem.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Controls;
+
+namespace Arc
+{
+    public class MainMenuItem
+    {
+        public string IconPath { get; set; }
+        public UserControl Control { get; set; }
+        
+        public MainMenuItem(string iconPath, UserControl control)
+        {
+            IconPath = iconPath;
+            Control = control;
+        }
+    }
+}
--- Arc/MainWindow.xaml.cs
+++ Arc/MainWindow.xaml.cs
@@ -1,18 +1,7 @@
-﻿using Arc.Xml;
-using System;
+﻿using Arc.UserControls;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace Arc
 {
@@ -21,9 +10,20 @@ namespace Arc
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        public List<MainMenuItem> MenuItems { get; set; }
+
         public MainWindow()
         {
-            InitializeComponent(); 
+            MenuItemMethod();
+            InitializeComponent();
+        }
+        void MenuItemMethod()
+        {
+            MenuItems = new List<MainMenuItem>();
+
+            MenuItems.Add(new MainMenuItem("/Resources/Images/Music_White.png", new SongLibrary()));
+
         }
     }
 }
