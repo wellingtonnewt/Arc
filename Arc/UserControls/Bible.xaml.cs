ee0ced4857262dabbc02e5f7ab7e1d96d2c755b9
Captain Newt <42378068+NNeerly@users.noreply.github.com>
2020-12-24 06:56:12 +0800

    Bible ViewModel

---
+++ Arc/UserControls/Bible.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Arc.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Arc.UserControls
+{
+    /// <summary>
+    /// Interaction logic for Bible.xaml
+    /// </summary>
+    public partial class Bible : UserControl
+    {
+        private BibleViewModel _viewmodel;
+
+        public Bible()
+        {
+            InitializeComponent();
+            _viewmodel = new BibleViewModel();
+            DataContext = _viewmodel;
+        }
+    }
+}
--- Arc/ViewModels/BibleViewModel.cs
+++ Arc/ViewModels/BibleViewModel.cs
@@ -8,16 +8,11 @@ using System.Text;
 
 namespace Arc.ViewModels
 {
-    public class MainWindowViewModel : INotifyPropertyChanged
+    public class BibleViewModel : INotifyPropertyChanged
     {
-        public ObservableCollection<MainMenuItem> MenuItems { get; set; }
 
-        public MainWindowViewModel()
+        public BibleViewModel()
         {
-            MenuItems = new ObservableCollection<MainMenuItem>();
-
-            MenuItems.Add(new MainMenuItem("/Resources/Images/Music_White.png", new SongLibrary()));
-            MenuItems.Add(new MainMenuItem("/Resources/Images/Bible_White.png", new Bible()));
         }
         public event PropertyChangedEventHandler PropertyChanged;
 
